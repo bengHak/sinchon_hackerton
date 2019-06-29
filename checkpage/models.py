12c6230424e5c8f4ce00db0956e69435df7479d6
leanne001 <leanne001@naver.com>
2019-06-29 23:38:58 +0900

    first commit

---
+++ checkpage/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+'''
+class Choice(models.Model):#Choice 객체 선언
+    name = models.CharField(max_length=100)
+    votes = models.IntegerField(default = 0)
+ #정수형 votes를 선언하고 해당 데이터의 기본값은 0임을 명시
+    q = models.ForeignKey(, on_delete=models.CASCADE)
+'''
